a2bd1f8efe2d5f0ffa140b58a0714fbcba35689e
Puja7629 <puja.hit01@gmail.com>
2020-06-26 22:56:48 +0530

    github token and get all profiles chnages updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,10 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
+import Routes from './components/routing/Routes';
 
-
-//Redux
-import {Provider} from 'react-redux';
+// Redux
+import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -22,34 +18,19 @@ const App = () => {
     store.dispatch(loadUser());
   }, []);
 
-
-return (
-  <Provider store= {store}>
-    <Router>
-    <Fragment>
-    
-    <Navbar />
-    <Route exact path="/" component={Landing} />
-    <section className ="container">
-      <Alert />
-      <Switch>
-      <Route exact path= '/register' component ={Register} />
-      <Route exact path= '/login' component ={Login} />
-      <Route exact path= '/dashboard' component ={Dashboard} />
-
-      </Switch>
-      
-    </section>
-
-    
-  </Fragment>
-  </Router>
-  </Provider>
-  
-  
-  
-    
-);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,7 +9,7 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 
-//@route    GET api/profile
+//@route    GET api/profile/me
 //@desc     Get current user profile
 //@acess Private
 router.get('/me',auth, async (req,res) => {
@@ -116,6 +116,20 @@ router.post(
     }
 );
 
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+      const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+      res.json(profiles);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
+
 // @route   GET api/profile/uswer/:user_id
 // @desc    GET profile by user_ID
 // @acess   Public
@@ -123,6 +137,7 @@ router.post(
 router.get('/user/:user_id',async(req, res)=>{
     try{
         const profile= await Profile.findOne({  user : req.params.user_id}).populate('user', ['name','avatar']);
+        
         if(!profile) 
         return res.status(400).json({ msg: 'Profile not fouond'});
 
@@ -358,42 +373,29 @@ router.delete('/education/:edu_id', auth, async(req, res)=>{
     }
 });
 
-/*
+
 // @route    GET api/profile/github/:username
 // @desc     Get user repos from Github
 // @access   Public
 router.get('/github/:username', async (req, res) => {
     try {
-      const options = {
-        uri: `https://api.github.com/users/${
-            req.params.username
-        }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-            'githubClientId'
-            )}&client_secret=${config.get('githubSecret')}`,
-            method: 'GET',
-            headers: {'user-agent': 'node.js'}
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
       };
-
-      request(options,(error, response, body) => {
-          if(error) console.error(error);
-          
-
-          if(response.statusCode!= 200){
-              return res.status(404).json({msg: "No Github profile found"});
-          }
-          res.json(JSON.parse(body));
-      });
-    }catch (err) {
-        console.err(err.message);
-        res.status(500).send('server Error');
-
-
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
     }
+  });
 
-      
-});
 
-*/
 
 
 module.exports = router;
